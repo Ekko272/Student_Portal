2ea824461d4ce45cea3ddb0e7ae146977ae1c97a
liuxuanyu <liuxuanyu2002@gmail.com>
2023-07-24 23:47:30 -0700

    completed Student class

--- src/main/java/com/example/demo/model/Student.java
+++ src/main/java/com/example/demo/model/Student.java
@@ -1,4 +1,38 @@
 package com.example.demo.model;
 
-public class Student {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Student extends User{
+    private List<Course> courseList;
+
+    public Student(){}
+    public Student(List<Course> courseList) {
+        this.courseList = new ArrayList<>();
+    }
+
+    public Student(int id, String userName, String password) {
+        super(id, userName, password);
+    }
+
+    public Student(String userName, String password) {
+        super(userName, password);
+    }
+
+    //once student has at least one course, allocate memory for ArrayList(saving memory compare to directly initialize courseList in property filed)
+    public void addCourse(Course course) {
+        if (courseList == null) {
+            courseList = new ArrayList<>();
+        }
+        courseList.add(course);
+    }
+
+    @Override
+    public String toString() {
+        String coursesInfo = null;
+        for (Course course : courseList) {
+            coursesInfo += course.toString();
+        }
+        return coursesInfo;
+    }
 }
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import com.example.demo.dao.AdminDao;
 import com.example.demo.model.Course;
+import com.example.demo.model.Student;
 import com.example.demo.model.User;
 import com.example.demo.service.AdminService;
 import org.junit.jupiter.api.Test;
@@ -20,6 +21,8 @@ class DemoApplicationTests {
     AdminService adminService;
     @Autowired
     AdminDao adminDao;
+    Course course1 = new Course(1,"1",2,3,"4","5",6,"5","8","8",5,"8","7","66",21);
+    Course course2 = new Course(312,"5",2,3,"4","5",6,"5","8","8",5,"8","7","66",21);
 
     @Test
     void contextLoads() {
@@ -58,5 +61,11 @@ class DemoApplicationTests {
     @Test
     void teeeeee(){
 
+        Student student = new Student("aa","bb");
+        student.addCourse(course1);
+        student.addCourse(course2);
+        System.out.println(student.toString());
     }
+
+
 }
